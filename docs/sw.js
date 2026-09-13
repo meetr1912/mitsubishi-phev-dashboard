f0745e84491e83b88d210a77a6bf813cfc9e43b9
Mit Patel <37380497+meetr1912@users.noreply.github.com>
2026-09-13 00:56:38 -0300

    chore(pwa): refresh climate control shell cache

--- docs/sw.js
+++ docs/sw.js
@@ -10,7 +10,7 @@
  * Bump CACHE_NAME whenever the shell asset list changes so activate() cleans
  * out the old version.
  */
-var CACHE_NAME = "phev-shell-v2";
+var CACHE_NAME = "phev-shell-v3";
 var SHELL_ASSETS = [
   "./",
   "./index.html",
